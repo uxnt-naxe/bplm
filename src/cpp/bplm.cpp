fb21fb2da04044eeb074e0140d8479e1f4d9fcc7
BeanFlame <beanflame@qq.com>
2024-03-03 01:30:43 +0800

    Add Code

--- src/cpp/bplm.cpp
+++ src/cpp/bplm.cpp
@@ -18,25 +18,53 @@ char* identifier[] = {
 };
 
 
-
+/*** slant - bplm
+ *        __          __        
+ *       / /_  ____  / /___ ___ 
+ *      / __ \/ __ \/ / __ `__ \
+ *     / /_/ / /_/ / / / / / / /
+ *    /_.___/ .___/_/_/ /_/ /_/ 
+ *         /_/                  
+ */
 
 
 int main(int argc, char *argv[]) {
 
     setlocale(LC_ALL, "");          // Linux MinGW GCC MSVC
     ios::sync_with_stdio(false);    // Linux gcc     
+
+    wcout <<
+R"(
+    __          __        
+   / /_  ____  / /___ ___ 
+  / __ \/ __ \/ / __ `__ \
+ / /_/ / /_/ / / / / / / /
+/_.___/ .___/_/_/ /_/ /_/ 
+     /_/                  
+     
+)";
+
+    // wcout << L"Uxnt Naxe 2021-2024" << endl;
+    wcout << L"bplm 0.0.1 (2024/3/4)" << endl;
+    wcout << L"" << endl;
+
+    int to=0;
+    for (const auto &element : identifier) {
+         wcout << "<id, " << identifier[to] << "> "  << endl;
+         to++;
+    }
+
+   // wcout << endl;
+
     // i18nString source = OtneReadFile("code.otne");
     // wcout << source << endl;
-
-    wcout << L"你好" << endl;
-    wcout << L"----------------------------------" << endl;
     // lexer(source);
     // return 0;
 
-    for(int to = 0; getLength(identifier) > to; to++) {
-        //<id, import>
-        wcout << "<id, " << identifier[to] << ">" << endl;
-    }
+    // for(int to = 0; getLength(identifier) > to; to++) {
+    //     //<id, import>
+    //     wcout << "<id, " << identifier[to] << ">" << endl;
+    // }
 
     return 0;
 }
